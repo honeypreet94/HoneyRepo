2b2fea7ea91aa5b13abde7029ed33168fafc512d
honeypreet94 <honeypreet94@gmail.com>
2020-06-02 15:30:04 +0530

    added succesfully

--- demo/src/main/java/com/example/demo/ApiController.java
+++ demo/src/main/java/com/example/demo/ApiController.java
@@ -1,11 +1,17 @@
 package com.example.demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
@@ -19,9 +25,25 @@ public class ApiController {
     }
 
     @RequestMapping(value="/api/temperatures")
-
+    @Produces(MediaType.APPLICATION_XML)
     public List<TempPojo> getTemp()
     {
         return al;
+    }
+
+
+    @RequestMapping(value="/api/temperatures/{id}")
+    public ResponseEntity<TempPojo> getTempById(@PathVariable int id)
+    {
+        Iterator iterator=al.iterator();
+        while(iterator.hasNext())
+        {
+            TempPojo obj=(TempPojo)iterator.next();
+            if(id==obj.id)
+                return new ResponseEntity<TempPojo>(obj, HttpStatus.ACCEPTED);
+
+        }
+
+        return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
     }
 }
