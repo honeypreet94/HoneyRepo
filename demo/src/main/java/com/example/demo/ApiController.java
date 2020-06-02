14d821902f516be5070cfe7c0681c0c3652801b9
honeypreet94 <honeypreet94@gmail.com>
2020-06-02 19:10:52 +0530

    added succesfully

--- demo/src/main/java/com/example/demo/ApiController.java
+++ demo/src/main/java/com/example/demo/ApiController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,6 +45,64 @@ public class ApiController {
 
         }
 
+        return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+    }
+
+    @RequestMapping(value="/api/temperatures/add")
+    public ResponseEntity<TempPojo> addTemp(@RequestBody TempPojo obj)
+    {
+        al.add(obj);
+        return new ResponseEntity<>(null,HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/api/temperatures/replace/{id}")
+    public ResponseEntity<TempPojo> replace(@PathVariable int id, @RequestBody TempPojo obj)
+    {
+        Iterator itr=al.iterator();
+        while(itr.hasNext())
+        {
+            TempPojo t=(TempPojo)itr.next();
+            if(id==t.id)
+            {
+                int i=al.indexOf(t);
+                al.set(i,obj);
+                return new ResponseEntity<TempPojo>(obj,HttpStatus.ACCEPTED);
+            }
+
+        }
+        return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+    }
+
+    @RequestMapping(value="/api/temperatures/delete/{id}")
+    public ResponseEntity<TempPojo> deleteData(@PathVariable int id)
+    {
+        Iterator itr=al.iterator();
+        while(itr.hasNext())
+        {
+            TempPojo t=(TempPojo)itr.next();
+            if(id==t.id)
+            {
+                int i=al.indexOf(t);
+                al.remove(i);
+                return new ResponseEntity<>(null,HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+    }
+    @RequestMapping(value="/api/temperatures/patch/{id}")
+    public ResponseEntity<TempPojo> replaceSpecific(@PathVariable int id, @RequestBody TempPojo obj)
+    {
+        Iterator itr=al.iterator();
+        while(itr.hasNext())
+        {
+            TempPojo t=(TempPojo)itr.next();
+            if(id==t.id)
+            {
+                t.setTemp(obj.getTemp());
+                return new ResponseEntity<TempPojo>(obj,HttpStatus.ACCEPTED);
+            }
+
+        }
         return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
     }
 }
